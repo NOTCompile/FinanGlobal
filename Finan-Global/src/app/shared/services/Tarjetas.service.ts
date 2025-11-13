2d7df7c1d7a1e664b42ad0157b78f5b0e1e561f2
NOTCompile <notcompile@gmail.com>
2025-11-13 08:58:02 -0500

    frontend/refactor/AdminModals - CuentasBancarias

---
+++ Finan-Global/src/app/shared/services/Tarjetas.service.ts
@@ -0,0 +1,12 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Usuario } from '../interfaces/Usuario-Interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class TarjetaService {
+    
+}
